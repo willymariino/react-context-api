c7cb4ddaca6e53e1ffc81e2caa8c4518acf0a1c9
willymariino <wg.mariino@gmail.com>
2025-04-26 19:25:46 +0200

    added postContext

--- src/pages/lista-post.jsx
+++ src/pages/lista-post.jsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import PostContext from "../contexts/PostContext"
 
 
 
