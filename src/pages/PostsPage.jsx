8f1476b7400dac9ed278e4a49313256ea82062d3
willymariino <wg.mariino@gmail.com>
2025-04-26 20:25:07 +0200

    fetchposts

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -4,10 +4,11 @@ import PostContext from "../contexts/PostContext"
 import axios from "axios"
 
 function fetchPosts() {
+    const { setPosts } = useContext(PostContext)
     axios.get("https://www.dnd5eapi.co/api/monsters")
         .then((res) => setPosts(res.data.results))
         .catch((error) => console.error("errore durante il caricamento", error))
-}
+} [setPosts]
 
 useEffect(fetchPosts, [])
 
