f3185c71b3d7cdc15ec51a1b919d9f4badd91c6c
willymariino <wg.mariino@gmail.com>
2025-04-26 20:17:12 +0200

    created postpage

---
+++ src/pages/PostsPage.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+import { useContext } from "react"
+import axios from "axios"
+
+function fetchPosts() {
+    axios.get("https://www.dnd5eapi.co/api/monsters")
+        .then((res) => setPosts(res.data.results))
+        .catch((error) => console.error("errore durante il caricamento", error))
+}
+
+useEffect(fetchPosts, [])
+
+export default fetchPosts
