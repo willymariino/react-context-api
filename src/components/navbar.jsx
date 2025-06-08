c671628dc24c19e458aa6b504dc5c9952f36fe61
willymariino <wg.mariino@gmail.com>
2025-06-08 17:14:04 +0200

    changed names pages

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,8 +4,8 @@ function Navbar() {
     return (
         <nav>
             <li> <NavLink to="/homepage" className="nav-link" >Home Page</NavLink> </li>
-            <li> <NavLink to="/about" className="nav-link">Chi Siamo</NavLink> </li>
-            <li> <NavLink to="/lista-post" className="nav-link">lista posts</NavLink> </li>
+            <li> <NavLink to="/about" className="nav-link">About Us</NavLink> </li>
+            <li> <NavLink to="/lista-post" className="nav-link">Posts List</NavLink> </li>
         </nav>
 
 
